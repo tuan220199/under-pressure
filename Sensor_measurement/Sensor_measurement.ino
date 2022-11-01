21862b0545f0fa4b46ae9eea794b8b9a534936db
tuan220199 <phamtuan.220199@gamil.com>
2022-11-02 03:21:51 +0700

    update arduino file 2 time

--- Sensor_measurement/Sensor_measurement.ino
+++ Sensor_measurement/Sensor_measurement.ino
@@ -6,15 +6,9 @@ uint8_t loopCnt = 0;
 Adafruit_Si7021 sensor = Adafruit_Si7021();
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   // wait for serial port to open
-  while (!Serial) {
-    delay(10);
-  }
-
-  Serial.println("Si7021 test!");
-  
   if (!sensor.begin()) {
     Serial.println("Did not find Si7021 sensor!");
     while (true)
@@ -50,7 +44,7 @@ void loop() {
 
   // Toggle heater enabled state every 30 seconds
   // An ~1.8 degC temperature increase can be noted when heater is enabled
-  if (++loopCnt == 30) {
+ /* if (++loopCnt == 30) {
     enableHeater = !enableHeater;
     sensor.heater(enableHeater);
     Serial.print("Heater Enabled State: ");
@@ -59,14 +53,15 @@ void loop() {
     else
       Serial.println("DISABLED");
        
-    loopCnt = 0;
+    loopCnt = 0;*/
     int sensorValue = analogRead(A0);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5.0 / 1023.0);
   // Print out the value you read:
+  Serail.print("Force");
   Serial.println(voltage);
   // Wait 100 milliseconds
-  delay(100);
+  delay(1000);
   }
 }
 
