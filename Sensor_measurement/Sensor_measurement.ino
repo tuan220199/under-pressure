1f29341ab2718389ba3512190b2efc5772f1a429
tuan220199 <phamtuan.220199@gamil.com>
2022-11-02 03:00:17 +0700

    update arduino file

--- Sensor_measurement/Sensor_measurement.ino
+++ Sensor_measurement/Sensor_measurement.ino
@@ -60,6 +60,13 @@ void loop() {
       Serial.println("DISABLED");
        
     loopCnt = 0;
+    int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  // Print out the value you read:
+  Serial.println(voltage);
+  // Wait 100 milliseconds
+  delay(100);
   }
 }
 
